13471f658ab8ba9f791d1b4e7ea93dff7cc5fbad
Desarrollo 4 <Desarrollo.4@interpagos.co>
2018-11-28 10:57:14 -0500

    sidebar, iconos

--- include/sidebar.php
+++ include/sidebar.php
@@ -1,3 +1,49 @@
 <div class="col-sm-3 col-md-3 sidebar">
+<div class="row">
+	<center>
+		<h2>Ranking</h2>
+	</center>
+	
+</div>
+<div class="row">
+	<div class="col-12">
+		<center>	
+			<ul class="list-ico">
+				<li><img src="images/iron.jpg" width="30" ></li>
+				<li><img src="images/bronce.jpg" width="30" ></li>
+				<li><img src="images/silver.jpg" width="30" ></li>
+				<li><img src="images/gold.jpg" width="30" ></li>
+				<li><img src="images/platinum.jpg" width="30" ></li>
+				<li><img src="images/diamond.jpg" width="30" ></li>
+			</ul>
+		</center>
+	</div>
+</div>
+<br>
+<div class="row">
+	<ul class="list">
+		<li>Puntaje Acumulado: </li>
+		<li>Puntaje para el siguiente nivel: </li>
+	</ul>
+</div>
+<br>
+<div class="row">
+	<center>
+		<img src="images/imagen.jpg" width="270">
+	</center>
+</div>
+<div class="row">
+	<div class="col-12">
+		<center>	
+			<ul class="list">
+				<li><img src="images/facebook_vintage.png" width="30" ></li>
+				<li><img src="images/instagram_vintage.png" width="30" ></li>
+			</ul>
+		</center>
+	</div>
+</div>
+
+
+
 
 </div>
