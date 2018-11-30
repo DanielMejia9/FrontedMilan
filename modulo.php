1f7fdbc22f52783032ffb77d3cdde71344401b1e
DESKTOP-9MIVFEN\Erain Moya <evmoya_89@hotmail.com>
2018-11-29 22:46:43 -0400

    Correcciones al archio modulo.php

--- modulo.php
+++ modulo.php
@@ -39,7 +39,6 @@ $activeClass= 0;
 <link href="css/dashboard.css" rel="stylesheet" type="text/css" />
 <link rel="stylesheet" type="text/css" href="css/component.css" />
 <script src="jscript/modernizr.custom.js"></script>
-<script src="jscript/funciones.js"></script>
 
 <title>Modulo</title>
 </head>
@@ -54,45 +53,36 @@ $activeClass= 0;
             ?>
 
             <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-            <div class="fondo">
-            <!--Div que contiene el tutilo de Acceso Directo
-                <div style="background:rgb(245, 245, 245); text-align:left; padding: 5px 0px 0px 10px; height:40px; border-bottom: 1px  #C0C0C0 solid;border-radius: 0px;">
-                    <span style="color: #003E55; font-weight:bold;font-size:14pt;">Acceso Directos </span>
-                </div>-->
-                <div class="row placeholders" >
-                                
-                </div>
-                <div class="row placeholders">
-                    <div class="row">
-                        <div class="class="col-12">
-                            <h2>Condiciones y Restricciones</h2>
-                        </div>
+                <div class="fondo">
+                    <div class="row placeholders" ></div>
+                    <div class="row placeholders">
+                        <div class="row">
+                            <div class="class="col-12">
+                                <h2>Condiciones y Restricciones</h2>
+                            </div>
                     </div>
                     <div class="row">
                         <div class="class="col-md-11">
                             <p>
-                               Para redimir los puntos el Socio de Milán B&C debe seguirnos en Facebook, e Instagram, etiquetar a 8 personas más en cada red social y llenar el formulario que se encuentra en www.milanbc.com . Los servicios del Plan de Puntos se realizarán de Lunes a Jueves de 9 AM a 1 PM, por cada servicio hasta el 50% del valor total podrá ser pagado con puntos. Bono por referidos. Por cada referido se sumaran 8 puntos a la cuenta del Socio de Milán B&C.
+                                Para redimir los puntos el Socio de Milán B&C debe seguirnos en Facebook, e Instagram, etiquetar a 8 personas más en cada red social y llenar el formulario que se encuentra en www.milanbc.com . Los servicios del Plan de Puntos se realizarán de Lunes a Jueves de 9 AM a 1 PM, por cada servicio hasta el 50% del valor total podrá ser pagado con puntos. Bono por referidos. Por cada referido se sumaran 8 puntos a la cuenta del Socio de Milán B&C. 
                             </p>
-                        </div>
                     </div>
-                </div>
-                <div class="row placeholders">
-                    <div class="row">
-                        <div class="class="col-12">
-                            <h2>Condiciones del Plan de Puntos</h2>
-                        </div>
+                    <div class="row placeholders">
+                        <div class="row">
+                            <div class="class="col-12">
+                                <h2>Condiciones del Plan de Puntos</h2>
+                            </div>
                     </div>
                     <div class="row">
                         <div class="class="col-md-11">
                             <p>
-                               El afiliado debería tener acceso a su historial, fotos antes y después de procedimientos, ofertas y promociones especiales, agendamiento de citas Deben estar cruzadas la información de facturación con la acumulación de puntos en cada cuenta, el colaborador que realizó el servicio o recomendó el producto
+                                El afiliado debería tener acceso a su historial, fotos antes y después de procedimientos, ofertas y promociones especiales, agendamiento de citas, deben estar cruzadas la información de facturación con la acumulación de puntos en cada cuenta, el colaborador que realizó el servicio o recomendó el producto
+
                             </p>
-                        </div>
                     </div>
                 </div>
+            </div>
         </div>
     </div>
-    </div>
- </div>   	
 </body>
 </html>
